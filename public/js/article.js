6e90667736f667ee6c1af14463d27620ef34bd2a
Abdurrahim Ahmadov <42205442+ebdurrehm@users.noreply.github.com>
2021-12-10 19:11:14 +0400

    Change baseUrl to avoid CORS error

--- public/js/article.js
+++ public/js/article.js
@@ -56,7 +56,7 @@ function addComment() {
     comment.insertBefore(elem, addComment);
     console.log(totalComment.value)
     totalComment.innerText = parseInt(totalComment.innerText) + 1;
-    axios.post('https://ahmadow.azurewebsites.net/comment', { id, name, email, text })
+    axios.post('http://ahmadow.azurewebsites.net/comment', { id, name, email, text })
         .then((response) => {
             console.log(response.data)
 
