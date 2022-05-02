93cfe6b658e2b4baad5913226e93432def31c02b
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-05-02 22:02:22 +0400

    correct admin authentication change middleware logic

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -1,12 +1,13 @@
 module.exports=function(req,res,next){
-    if(req.session.isAdmin){
+    if(req.session.isAdmin ==='true'){
        
         next();
         
     } else{
         res.status(401);
         res.send('you have not permission');
+       
     }
-    next();
+    
     
 }
