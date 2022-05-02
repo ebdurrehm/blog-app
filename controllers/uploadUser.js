fc5e54cf7532add2af967bff67bb454b7f25bbd4
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-05-02 22:13:59 +0400

    delete some unnecessary comments and codes save the new user's id and isAdmin property to the session

--- controllers/uploadUser.js
+++ controllers/uploadUser.js
@@ -1,22 +1,26 @@
-var User = require("../database/models/User");
+const User = require("../database/models/User");
 
-module.exports = (req, res)=>{
-    if(req.body.isAdmin){
+module.exports= (req, res) => {
+    if (req.body.isAdmin) {
         req.body.isAdmin = true;
     }
-    User.create(req.body, (error, user)=>{
+    else{
+        req.body.isAdmin = false;
+    }
+    
+    User.create(req.body, (error, user) => {
         console.log(req.body)
-       if(error){
-        console.log(error);
+        if (error) {
+            console.log(error);
 
-        // const registrationErrors = Object.keys(error.errors).map(key=>error.errors[key].message);
-        // req.flash('registrationErrors',registrationErrors);
-           return  res.redirect('/user/register');
-           
-       }
-       req.session.userId =user._id;
-       req.session.isAdmin=user.isAdmin;
-       console.log(req.session)
-       res.redirect("/");
+
+            return res.redirect('/user/register');
+
+        }
+        req.session.userId = user._id;
+        req.session.isAdmin = user.isAdmin;
+        console.log(req.session)
+        res.redirect("/");
     })
 }
+
