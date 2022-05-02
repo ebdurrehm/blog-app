6ff3f1fad5761a67a013a975344fb490adb5d0d6
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-05-02 21:58:50 +0400

    database connection

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");// for to connect database
 async function connectDb(){
 dotenv.config({ path: 'secret.env' });
 //connect to database
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log("you are connected");
     })
