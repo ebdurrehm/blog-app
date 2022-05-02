39a0153c4a8a7a3f401f3db449c63a2b34d59a8f
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-05-02 22:22:30 +0400

    create auth middleware to be sure that the user logged in

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 
 module.exports = (req, res, next)=>{
     
-    if (req.session.userId){
-        res.redirect('/');
+    if (!req.session.userId){
+        res.send('you are not logged');
     }else{
         next();
     }
