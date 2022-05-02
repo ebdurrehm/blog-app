3072ebe597e47fa16d6abf0c4c98e8463532e052
Abdurrahim Ahmadov <ebdurrehim5@gmail.com>
2022-05-02 22:06:49 +0400

    get valid user informations and fill comments cards with them.

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,9 +1,10 @@
 var Data = require("../database/models/Data");
-const User = require("../database/models/User");
+
 
 module.exports=  async (req,res)=>{
     const posts = await Data.findOne({"slug":req.params.slug});
-    res.render('article',{posts, comments: posts.comment.sort(function(a,b){
+    
+    res.render('article',{posts, user:req.user, comments: posts.comment.sort(function(a,b){
         return b.createdAt-a.createdAt;
       })})};
     
